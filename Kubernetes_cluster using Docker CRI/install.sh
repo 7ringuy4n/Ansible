03d36923ea6394dc7901da15c907f645f5a67b18
johnninja54 <131328514+johnninja54@users.noreply.github.com>
2023-05-08 08:34:00 +0700

    Update install.sh

--- Kubernetes_cluster using Docker CRI/install.sh
+++ Kubernetes_cluster using Docker CRI/install.sh
@@ -103,7 +103,7 @@ apt-get install -y kubelet=1.26.1-00 kubeadm=1.26.1-00 kubectl=1.26.1-00
 systemctl enable kubelet
 
 #Setup MasterNode
-kubeadm config images pull --cri-socket /run/cri-dockerd.sock
+#kubeadm config images pull --cri-socket /run/cri-dockerd.sock
 kubeadm init \
     --apiserver-advertise-address=$controlip \
     --apiserver-cert-extra-sans=$controlip \
@@ -130,70 +130,4 @@ curl https://raw.githubusercontent.com/projectcalico/calico/v3.25.1/manifests/ca
 kubectl apply -f calico.yaml
 kubectl apply -f https://raw.githubusercontent.com/techiescamp/kubeadm-scripts/main/manifests/metrics-server.yaml
 sleep 30
-kubectl get nodes -o wide
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#option1
-swapoff -a
-systemctl mask swap.target
-apt-get update
-apt-get install -y apt-transport-https ca-certificates curl
-mkdir -m 0755 -p /etc/apt/keyrings
-curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
-
-curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s \
-https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
-
-chmod +x ./kubectl
-mv ./kubectl /usr/local/bin/kubectl
-
-apt-get update
-apt-get install -y kubelet=1.26.1-00 kubeadm=1.26.1-00 kubectl=1.26.1-00
-systemctl enable kubelet
-
-#Setup MasterNode
-kubeadm init \
-    --apiserver-advertise-address=$controlip \
-    --apiserver-cert-extra-sans=$controlip \
-    --pod-network-cidr=$pod_cidr \
-    --service-cidr=$service_cidr \
-    --node-name=$hostname \
-    --cri-socket /run/containerd/containerd.sock \
-    --ignore-preflight-errors Swap >> /home/adminconfig.txt
-
-tail -2 /home/adminconfig.txt >> ./script/join.sh
-
-export KUBECONFIG=/etc/kubernetes/admin.conf
-cp -i /etc/kubernetes/admin.conf ./script/config
-chown -R $user:$user ./script/
-chmod -R 777 ./script/
-chmod 655 /etc/kubernetes/admin.conf
-mkdir -p /home/$user/.kube
-cp -i /etc/kubernetes/admin.conf /home/$user/.kube/config
-chown $user:$user /home/$user/.kube/config
-
-#Run Ansible
-ansible-playbook Setup.yml -l Nodes --become --ask-become-pass
-
-#install Calico CNI
-curl https://raw.githubusercontent.com/projectcalico/calico/v3.25.1/manifests/calico.yaml -O
-kubectl apply -f calico.yaml
-kubectl apply -f https://raw.githubusercontent.com/techiescamp/kubeadm-scripts/main/manifests/metrics-server.yaml
-sleep 120
-kubectl label node node01 node-role.kubernetes.io/worker=worker
-kubectl apply -f ./script/testwebserver.yml
 kubectl get nodes -o wide
