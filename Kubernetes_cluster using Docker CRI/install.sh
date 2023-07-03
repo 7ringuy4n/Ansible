40dab22ecedb02f1ed2a42aaf053c4373092ad81
7ringuy4n <132858234+7ringuy4n@users.noreply.github.com>
2023-07-03 20:24:49 +0700

    Update install.sh

--- Kubernetes_cluster using Docker CRI/install.sh
+++ Kubernetes_cluster using Docker CRI/install.sh
@@ -89,11 +89,10 @@ systemctl mask swap.target
 apt-get update
 apt-get install -y apt-transport-https ca-certificates curl
 mkdir -m 0755 -p /etc/apt/keyrings
-curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
 
-curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s \
-https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
+curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
 
 chmod +x ./kubectl
 mv ./kubectl /usr/local/bin/kubectl
